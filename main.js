90947deceff1f3bfd72c1e0ac4edc8a0b33e4765
ibraheemalquraishy <ibraheemalquraishy22211123@gmail.com>
2022-05-31 18:18:56 +0300

    fixing css and imgs

--- main.js
+++ main.js
@@ -7,8 +7,23 @@ const app=express();
 app.get('/',(req,res)=>{
     res.statusCode=200;
     res.sendFile(path.join(__dirname+'/index.html'));
-    
 
+})
+
+app.get('/css/main.css',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/css'+'/main.css'))
+})
+
+app.get('/imgs/left.png',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/imgs'+'/left.png'))
+})
+
+app.get('/imgs/logo.jpg',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/imgs'+'/logo.jpg'))
+})
+
+app.get('/css/404.css',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/css'+'/404.css'))
 })
 
 app.get('/about',(req,res)=>{
