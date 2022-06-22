1ab6a1fa6822a11d32040b7f077c727e7627c819
ibraheemalquraishy <ibraheemalquraishy22211123@gmail.com>
2022-06-22 10:49:38 +0300

    downloading function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express=require('express');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const path=require('path');
+const fs=require('fs');
 const HOSTNAME= process.env.HOSTNAME || "localhost";
 const PORT= process.env.PORT || 3000;
 const app=express();
@@ -39,9 +40,14 @@ app.get('/projectlists',(req,res)=>{
     res.sendFile(path.join(__dirname+'/projects'+'/info.json'));
 })
 
+app.get('/projects/patient_manager',(req,res)=>{
+    res.statusCode=200;
+    res.download(path.resolve(__dirname+'/projects/patient_manager'));
+})
+
 app.get('/test',(req,res)=>{
     res.statusCode=200;
-    res.sendFile(path.join(__dirname+'/test.html'))
+    res.sendFile(path.join(__dirname+"/test.html"))
 })
 
 app.all('*',(req,res)=>{
